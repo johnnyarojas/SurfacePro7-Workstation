7a32911f63d4ffad9d2f919e28eacf3b1cfcd396
johnnyarojas <johnny.rojas.me@gmail.com>
2026-01-26 22:35:15 -0500

    Added Home Manager on flake.nix, basic config on home.nix with jupyerlab

--- flake.nix
+++ flake.nix
@@ -4,9 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:8bitbuddhist/nixos-hardware/surface-kernel-6.18";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }:
+  outputs = { self, nixpkgs, nixos-hardware, home-manager }:
   {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
@@ -14,6 +19,12 @@
         modules = [
           ./configuration.nix
 	  nixos-hardware.nixosModules.microsoft-surface-pro-intel
+	  home-manager.nixosModules.home-manager
+	  {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.johnny = import ./home.nix;
+          }
         ];
       };
     };

bb5002f548a7fbc0581bba2316067abe7d95f9d4
johnnyarojas <johnny.rojas.me@gmail.com>
2026-01-26 22:38:10 -0500

    Added home.nix that was left over

---
+++ home.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  home.stateVersion = "25.11";
+
+  home.packages = with pkgs; [
+    python311
+    python311Packages.jupyterlab
+    python311Packages.numpy
+    python311Packages.pandas
+    python311Packages.matplotlib
+  ];
+
+  programs.home-manager.enable = true;
+}

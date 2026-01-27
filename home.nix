4cefa00da4847e77ee772576d4f212f1075a2b17
johnnyarojas <johnny.rojas.me@gmail.com>
2026-01-27 00:21:26 -0500

    Added Implementations for the Kernel and Vim Bindings of JupyterLab of the Thinkpad-WS

--- home.nix
+++ home.nix
@@ -4,15 +4,17 @@
   home.stateVersion = "25.11";
 
   home.packages = with pkgs; [
-    python311
-    python311Packages.jupyterlab
-    python311Packages.ipykernel
-    python311Packages.numpy
-    python311Packages.pandas
-    python311Packages.matplotlib
+    (
+      python311.withPackages (pythonPackages: [
+        pythonPackages.jupyterlab
+	pythonPackages.jupyterlab-vim
+        pythonPackages.ipykernel
+        pythonPackages.numpy
+        pythonPackages.pandas
+        pythonPackages.matplotlib
+      ])
+    )
   ];
 
   programs.home-manager.enable = true;
 }
-
-

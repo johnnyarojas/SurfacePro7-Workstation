b3c477f1eadb40726e7778b937b719d2005898a4
johnnyarojas <johnny.rojas.me@gmail.com>
2026-02-05 00:04:01 -0500

    Applied Modularization

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
 	  {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.johnny = import ./home.nix;
+            home-manager.users.johnny = import ./system/users/johnny/home.nix;
           }
         ];
       };
